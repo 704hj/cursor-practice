e53e38a9c921184eaa1773d3189e7d37cf17060d
704hj@dataeum.kr <704hj@dataeum.kr>
2025-10-27 21:43:43 +0900

    [UPDATE] 而ㅻ컠 洹쒖튃 ?쒓? ?몄퐫???ㅼ젙 異붽?
    - Windows ?섍꼍 PowerShell ?섍꼍 蹂???ㅼ젙 異붽?
    - git core.quotepath ?듭뀡 異붽?
    - i18n.commitencoding UTF-8 ?ㅼ젙

--- app/(view)/(anyone)/practice/feat.tsx
+++ app/(view)/(anyone)/practice/feat.tsx
@@ -1,6 +1,6 @@
 // [FEAT] 사용자 인사 기능 추가
-function greetUser(name: string) {
+function greetUser(name: string, age: number) {
   return `안녕하세요, ${name}님!`;
 }
 
-console.log(greetUser("왜 안될까?"));
+console.log(greetUser("왜 안될까?", 20));
